f364f73ebf39d0f82499d69e8138c67a14da89f6
suvrotica Ghosh <suvro.ghosh.contact@gmail.com>
2024-01-09 14:59:06 +0000

    v2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
+</script>
